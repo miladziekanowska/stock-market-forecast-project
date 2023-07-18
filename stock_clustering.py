bd30890bf8cf1d533c3f1df2a8a989bfd43efa93
Mila <miloslawadziekanowska@gmail.com>
2023-07-18 20:44:15 +0200

    stock_dataset.py, stock_clustering.py and initial dataset added

---
+++ stock_clustering.py
@@ -0,0 +1,61 @@
+# importing libraries
+import pandas as pd
+import numpy as np
+from tslearn.clustering import TimeSeriesKMeans
+from sklearn.preprocessing import StandardScaler
+
+# importing our data
+from stock_dataset import stock_data
+
+# for scaling the data
+scaler = StandardScaler()
+
+# Preparing the data to be clustered
+for key in stock_data:
+    stock_data[key] = stock_data[key][['close', 'dailychange']]
+    stock_data[key] = scaler.fit_transform(stock_data[key])
+
+X = np.array(list(stock_data.values()))
+
+
+# using the TimeSeriesKMeans model for clustering
+kmeans = TimeSeriesKMeans(n_clusters=5,
+                         n_init=5)
+
+# Fitting the data
+kmeans.fit(X)
+
+# Creating the labels variable
+labels = kmeans.labels_
+
+# Creating the clusters dictionary for future use
+clusters_dict = {}
+
+for i, firm_name in enumerate(stock_data.keys()):
+    if labels[i] == 0:
+        if 0 not in clusters_dict.keys():
+            clusters_dict[0] = [firm_name]
+        else:
+            clusters_dict[0].append(firm_name)
+    elif labels[i] == 1:
+        if 1 not in clusters_dict.keys():
+            clusters_dict[1] = [firm_name]
+        else:
+            clusters_dict[1].append(firm_name)
+    elif labels[i] == 0:
+        if 2 not in clusters_dict.keys():
+            clusters_dict[2] = [firm_name]
+        else:
+            clusters_dict[2].append(firm_name)
+    elif labels[i] == 3:
+        if 3 not in clusters_dict.keys():
+            clusters_dict[3] = [firm_name]
+        else:
+            clusters_dict[3].append(firm_name)
+    elif labels[i] == 4:
+        if 4 not in clusters_dict.keys():
+            clusters_dict[4] = [firm_name]
+        else:
+            clusters_dict[4].append(firm_name)
+            
+print('Clustering done!')
---
+++ stock_dataset.py
@@ -0,0 +1,46 @@
+# pip install yahoo_fin
+
+# importing libraries and modules
+import pandas as pd
+import numpy as np
+import yahoo_fin.stock_info as yh
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import warnings
+
+warnings.filterwarnings("ignore", message="Parsing dates")
+
+# Creating dictionary with stock prices of Dow Jones stocks from last 3 years
+ticker_list = yh.tickers_dow()
+initial_data = {}
+
+start_date = (datetime.now() - relativedelta(years=3)).strftime('%d-%m-%Y')
+end_date = datetime.today().strftime('%d-%m-%Y')
+
+for ticker in ticker_list:
+    initial_data[ticker] = yh.get_data(ticker, start_date=start_date, end_date=end_date, index_as_date = False,
+                                           interval="1d")
+
+# creating a new feature with the proprtion of the day's closing price to the previous day's closing price
+for key in initial_data:
+    initial_data[key]['dailychange'] = initial_data[key]['close'].div(initial_data[key]['close'].shift(1))
+    initial_data[key]['dailychange'].fillna(1, inplace=True)
+
+# creating the desired form of the data
+stock_data = {}
+for key in initial_data:
+    stock_data[key] = initial_data[key][['date', 'close', 'dailychange']]
+
+
+# creating dataframe with all data for visualization
+data_dict = {}
+
+for company, dataset in initial_data.items():
+    data_dict[company] = dataset.assign(company_name=company)
+
+dataset = pd.concat(data_dict.values()).reset_index(drop=True)
+
+dataset.to_csv('data/stock_initial_data.csv')
+
+
+print('Stock data ready to use!')
